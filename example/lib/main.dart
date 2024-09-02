eaa87c10b1cd52223855bfe3375956f8176df656
10583 <liudongcai@addcn.com>
2024-09-02 16:13:34 +0800

    Provide date picker

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:example/date_picker_demo.dart';
 import 'package:example/date_range_picker_demo.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
-import 'package:date_picker_pro/date_picker.dart';
 
 void main() => runApp(MyApp());
 
